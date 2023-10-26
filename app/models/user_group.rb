b30fb40ac0c8f4c8e33794ed1e0fde86ce4dcadd
garri-figueroa <garri.figueroa@aimmanager.com>
2023-10-26 17:50:21 -0300

    TDIR-27 script and model amendment

--- app/models/user_group.rb
+++
@@ -1,5 +0,0 @@
-class UserGroup < ApplicationRecord
-  self.table_name = "aim_users_groups"
-  belongs_to :group
-  belongs_to :user
-end
--- spec/factories/user_groups.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :user_group do
-    add_attribute(:group) { nil }
-    add_attribute(:user) { nil }
-    add_attribute(:created_at) { "2023-10-23 12:29:26" }
-    add_attribute(:updated_at) { "2023-10-23 12:29:26" }
-  end
-end
